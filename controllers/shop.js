70312df4296db1e823a1c1af06a2b4e8198f1e96
Ashish Ahuja <ashish.ahuja@locusnine.com>
2020-08-26 14:27:52 +0530

    generating pdf(invoice) using pdfkit

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,7 +2,7 @@ const Product = require("../models/product");
 const Order = require("../models/order");
 const fs = require('fs');
 const path = require('path');
-
+const PDFDocument = require('pdfkit');
 exports.getProducts = (req, res, next) => {
   Product.find()
     .then((products) => {
@@ -164,6 +164,35 @@ exports.getInvoice = (req, res, next) => {
       }
       const invoiceName = 'invoice-' + orderId + '.pdf';
       const invoicePath = path.join('data', 'invoices', invoiceName);
+
+      const pdfDoc = new PDFDocument();
+      res.setHeader('Content-Type', 'application/pdf');
+      res.setHeader(
+        'Content-Disposition',
+        'inline; filename="' + invoiceName + '"'
+      );
+      pdfDoc.pipe(fs.createWriteStream(invoicePath));
+      pdfDoc.pipe(res);
+      pdfDoc.fontSize(26).text('Invoice',{
+        underline: true
+      });
+
+      pdfDoc.text('----------------------');
+      let totalPrice = 0;
+      order.products.forEach(prod => {
+        totalPrice += prod.quantity * prod.product.price;
+        pdfDoc.fontSize(14).text(
+          prod.product.title 
+          + '-' 
+          + prod.quantity
+          + '*'
+          + '$'
+          + prod.product.price
+          );
+      });
+
+      pdfDoc.fontSize(20).text('Total Price: $' + totalPrice);
+      pdfDoc.end();
       // fs.readFile(invoicePath, (err, data) => {
       //   if (err) {
       //     return next(err);
@@ -175,13 +204,9 @@ exports.getInvoice = (req, res, next) => {
       //   );
       //   res.send(data);
       // });
-      const file = fs.createReadStream(invoicePath); //Use stream because reading a file will take larger time
-      res.setHeader('Content-Type', 'application/pdf');
-      res.setHeader(
-        'Content-Disposition',
-        'inline; filename="' + invoiceName + '"'
-      );
-      file.pipe(res);
+      // const file = fs.createReadStream(invoicePath); //Use stream because reading a file will take larger time
+      
+      // file.pipe(res);
     })
     .catch(err => next(err));
 };
